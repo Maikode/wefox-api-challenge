a3d20f1166286e7eb6f0c8a62e88fabc6a44daba
MiguelRG <mrobles@gmx.es>
2022-06-07 12:57:24 +0200

    feat: refactor address adding checking input params

--- src/context/address/domain/exceptions/address_not_found.ts
+++ src/context/address/domain/exceptions/address_not_found.ts
@@ -1,5 +1,5 @@
 export class AddressNotFoundError extends Error {
     constructor() {
-        super('Invalid address, there are missing or bad parameters');
+        super('Invalid address');
     }
 }
--- src/context/address/infrastructure/validate_address_controller.ts
+++ src/context/address/infrastructure/validate_address_controller.ts
@@ -4,14 +4,36 @@ import { AddressNotFoundError } from '../domain/exceptions/address_not_found';
 
 export const validateAddressController = async (req: Request, res: Response) => {
     const { street, streetNumber, town, postalCode, country } = req.query;
-    try {
-        const address = await validateAddressUseCase.validate(street, streetNumber, town, postalCode, country);
-        res.status(200).send(address).json();
-    } catch (error) {
-        if (error instanceof AddressNotFoundError) {
-            res.status(404).send({ error: error.message }).json();
-        } else {
-            res.status(500).send('Internal server Error');
+    if (!checkBadParams(street, streetNumber, town, postalCode, country)) {
+        res.status(400).send('There are missing or bad parameters to validate address');
+    } else {
+        try {
+            const address = await validateAddressUseCase.validate(street, streetNumber, town, postalCode, country);
+            res.status(200).send(address).json();
+        } catch (error) {
+            if (error instanceof AddressNotFoundError) {
+                res.status(404).send({ error: error.message }).json();
+            } else {
+                res.status(500).send('Internal server Error');
+            }
         }
     }
+};
+
+const checkBadParams = (street: any, streetNumber: any, town: any, postalCode: any, country: any): boolean => {
+    if (
+        street === undefined ||
+        street === '' ||
+        streetNumber === undefined ||
+        streetNumber === '' ||
+        town === undefined ||
+        town === '' ||
+        postalCode === undefined ||
+        postalCode === '' ||
+        country === undefined ||
+        country === ''
+    ) {
+        return false;
+    }
+    return true;
 };
